a8143dbe48dac18644b7ea5d04cfe8a2a8422aec
bokuyemskyy <i20808753@gmail.com>
2026-01-27 11:28:52 +0100

    chore: merge fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,33 @@
-#include <glad/glad.h>
+#include <glad/gl.h>
+
 #include <GLFW/glfw3.h>
 #include <iostream>
 
 int main(void) {
-    GLFWwindow *window;
+  GLFWwindow *window;
+  if (!glfwInit())
+    return -1;
 
-    if (!glfwInit())
-        return -1;
-
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-
-    while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
+  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (!window) {
     glfwTerminate();
-    return 0;
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  int version = gladLoadGL(glfwGetProcAddress);
+  if (version == 0) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
 }
